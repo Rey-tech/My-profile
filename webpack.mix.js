f267205d06843955990de40e40c8fd558e1e5cc2
Rey-tech <reytoday.sa@gmail.com>
2021-06-01 15:33:15 +0200

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+const mix = require("laravel-mix")
+mix.sass("src/scss/style.scss","public/css/style.css")
+    .js("src/js/script.js","public/js/script.js")
+    .options({
+        processCssUrls: false
+    })
